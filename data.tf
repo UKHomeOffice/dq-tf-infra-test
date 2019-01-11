0caed3ef855b61b40379754a54b486a9a0e6e595
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-01-11 13:04:37 +0000

    Comment out peering connections

--- data.tf
+++ data.tf
@@ -1,10 +1,10 @@
-#data "aws_vpc_peering_connection" "ad_peering_with_ops" {
-#  provider      = "aws.APPS"
-#  vpc_id        = "${module.ops.opsvpc_id}"
-#  owner_id      = "${data.aws_caller_identity.apps.account_id}"
-#  peer_vpc_id   = "${module.ad.vpc_id}"
-#  peer_owner_id = "${data.aws_caller_identity.apps.account_id}"
-#}
+data "aws_vpc_peering_connection" "ad_peering_with_ops" {
+  provider      = "aws.APPS"
+  vpc_id        = "${module.ops.opsvpc_id}"
+  owner_id      = "${data.aws_caller_identity.apps.account_id}"
+  peer_vpc_id   = "${module.ad.vpc_id}"
+  peer_owner_id = "${data.aws_caller_identity.apps.account_id}"
+}
 
 data "aws_vpc_peering_connection" "ad_peering" {
   provider      = "aws.APPS"
@@ -14,21 +14,21 @@ data "aws_vpc_peering_connection" "ad_peering" {
   peer_owner_id = "${data.aws_caller_identity.apps.account_id}"
 }
 
-data "aws_vpc_peering_connection" "ops_to_acpvpn" {
-  provider = "aws.APPS"
+#data "aws_vpc_peering_connection" "ops_to_acpvpn" {
+#  provider = "aws.APPS"
 
-  tags {
-    Name = "ops-to-vpn"
-  }
-}
+#  tags {
+#    Name = "ops-to-vpn"
+#  }
+#}
 
-data "aws_vpc_peering_connection" "peering_to_acp" {
-  provider = "aws.APPS"
+#data "aws_vpc_peering_connection" "peering_to_acp" {
+#  provider = "aws.APPS"
 
-  tags {
-    Name = "peering-to-acp"
-  }
-}
+#  tags {
+#    Name = "peering-to-acp"
+#  }
+#}
 
 data "aws_caller_identity" "apps" {
   provider = "aws.APPS"
