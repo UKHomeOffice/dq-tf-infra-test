09c0dd8cb49e9c32a19a0cb5a36eba50cf48548f
Jwanderema <wanderema@hotmail.co.uk>
2020-04-24 03:18:31 +0100

    clone

--- data.tf
+++ data.tf
@@ -14,21 +14,21 @@ data "aws_vpc_peering_connection" "ad_peering" {
   peer_owner_id = "${data.aws_caller_identity.apps.account_id}"
 }
 
-#data "aws_vpc_peering_connection" "ops_to_acpvpn" {
-#  provider = "aws.APPS"
+data "aws_vpc_peering_connection" "ops_to_acpvpn" {
+  provider = "aws.APPS"
 
-#  tags {
-#    Name = "ops-to-vpn"
-#  }
-#}
+  tags {
+    Name = "ops-to-vpn"
+  }
+}
 
-#data "aws_vpc_peering_connection" "peering_to_acp" {
-#  provider = "aws.APPS"
+data "aws_vpc_peering_connection" "peering_to_acp" {
+  provider = "aws.APPS"
 
-#  tags {
-#    Name = "peering-to-acp"
-#  }
-#}
+  tags {
+    Name = "peering-to-acp"
+  }
+}
 
 data "aws_caller_identity" "apps" {
   provider = "aws.APPS"
@@ -38,24 +38,24 @@ data "aws_caller_identity" "ci" {
   provider = "aws.CI"
 }
 
-#data "aws_kms_secret" "ad_joiner_password" {
-#  secret {
-#    name    = "ad_joiner_password"
-#    payload = "AQICAHhbM/ukRBb/Ya4cVR7Kqv0R4PfUfoNysP2u4BpIel0PVwHrIRsrc3RgFS47HkQEpFGIAAAAbjBsBgkqhkiG9w0BBwagXzBdAgEAMFgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMVB0fwYpQFnROpCbJAgEQgCvxRH6nYRagTHqOlgCUjldFyCxOc///2mVoAxNFFPp3+d7ZOtqqyjN3pTaB"
-#
-#    context {
-#      terraform = "active_directory"
-#    }
-#  }
-#}
+data "aws_kms_secrets" "ad_joiner_password" {
+  secret {
+    name    = "ad_joiner_password"
+    payload = "AQICAHhbM/ukRBb/Ya4cVR7Kqv0R4PfUfoNysP2u4BpIel0PVwHrIRsrc3RgFS47HkQEpFGIAAAAbjBsBgkqhkiG9w0BBwagXzBdAgEAMFgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMVB0fwYpQFnROpCbJAgEQgCvxRH6nYRagTHqOlgCUjldFyCxOc///2mVoAxNFFPp3+d7ZOtqqyjN3pTaB"
 
-#data "aws_kms_secret" "ad_admin_password" {
-#  secret {
-#    name    = "ad_admin_password"
-#    payload = "AQICAHhbM/ukRBb/Ya4cVR7Kqv0R4PfUfoNysP2u4BpIel0PVwEgo9lW1FCt6JnG/R5VFAKsAAAAbjBsBgkqhkiG9w0BBwagXzBdAgEAMFgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMSlbmCn+wejXdA1d0AgEQgCs5uNKKe38Zl9flh3hDGmLFUiqoOnJNGprDTbarqqGF4Gqv0uW8QmqaT502"
+    context {
+      terraform = "active_directory"
+    }
+  }
+}
 
- #   context {
- #     terraform = "active_directory"
- #   }
- # }
-#}
+data "aws_kms_secrets" "ad_admin_password" {
+  secret {
+    name    = "ad_admin_password"
+    payload = "AQICAHhbM/ukRBb/Ya4cVR7Kqv0R4PfUfoNysP2u4BpIel0PVwEgo9lW1FCt6JnG/R5VFAKsAAAAbjBsBgkqhkiG9w0BBwagXzBdAgEAMFgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMSlbmCn+wejXdA1d0AgEQgCs5uNKKe38Zl9flh3hDGmLFUiqoOnJNGprDTbarqqGF4Gqv0uW8QmqaT502"
+
+    context {
+      terraform = "active_directory"
+    }
+  }
+}
