a0b1b1d2dc27e830bc7ed066e1516731b6de46f3
Jwanderema <wanderema@hotmail.co.uk>
2020-04-25 21:11:01 +0100

    add dummy payloads

--- data.tf
+++ data.tf
@@ -41,7 +41,7 @@ data "aws_caller_identity" "ci" {
 data "aws_kms_secrets" "ad_joiner_password" {
   secret {
     name    = "ad_joiner_password"
-    payload = "AQICAHhbM/ukRBb/Ya4cVR7Kqv0R4PfUfoNysP2u4BpIel0PVwHrIRsrc3RgFS47HkQEpFGIAAAAbjBsBgkqhkiG9w0BBwagXzBdAgEAMFgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMVB0fwYpQFnROpCbJAgEQgCvxRH6nYRagTHqOlgCUjldFyCxOc///2mVoAxNFFPp3+d7ZOtqqyjN3pTaB"
+    payload = "AQICAHjC4CKFKTYsLki4xGd1rSMF7wMNMTmrxkezpb60vXSGtwGe2oPXUcDg/4HPEaxPaLuuAAAAcDBuBgkqhkiG9w0BBwagYTBfAgEAMFoGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMeCVXGJ7AUltYlqFRAgEQgC3NtnY3hGkL4qbo4aGyjYLrHODz+BCUUubQ5oC3PyiyJcL0z+7auFvAFAnMJzc="
 
     context {
       terraform = "active_directory"
@@ -52,7 +52,7 @@ data "aws_kms_secrets" "ad_joiner_password" {
 data "aws_kms_secrets" "ad_admin_password" {
   secret {
     name    = "ad_admin_password"
-    payload = "AQICAHhbM/ukRBb/Ya4cVR7Kqv0R4PfUfoNysP2u4BpIel0PVwEgo9lW1FCt6JnG/R5VFAKsAAAAbjBsBgkqhkiG9w0BBwagXzBdAgEAMFgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMSlbmCn+wejXdA1d0AgEQgCs5uNKKe38Zl9flh3hDGmLFUiqoOnJNGprDTbarqqGF4Gqv0uW8QmqaT502"
+    payload = "AQICAHjC4CKFKTYsLki4xGd1rSMF7wMNMTmrxkezpb60vXSGtwGe2oPXUcDg/4HPEaxPaLuuAAAAcDBuBgkqhkiG9w0BBwagYTBfAgEAMFoGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMeCVXGJ7AUltYlqFRAgEQgC3NtnY3hGkL4qbo4aGyjYLrHODz+BCUUubQ5oC3PyiyJcL0z+7auFvAFAnMJzc="
 
     context {
       terraform = "active_directory"
