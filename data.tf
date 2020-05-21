37d3df4191375735b31cd03da45362dc1d220cbe
Thet Naing <thet.naing@digital.homeoffice.gov.uk>
2020-05-21 13:03:00 +0100

    terraform code upgraded to 0.12 with modified test cases

--- data.tf
+++ data.tf
@@ -1,17 +1,17 @@
 data "aws_vpc_peering_connection" "ad_peering_with_ops" {
-  provider      = "aws.APPS"
-  vpc_id        = "${module.ops.opsvpc_id}"
-  owner_id      = "${data.aws_caller_identity.apps.account_id}"
-  peer_vpc_id   = "${module.ad.vpc_id}"
-  peer_owner_id = "${data.aws_caller_identity.apps.account_id}"
+  provider      = aws.APPS
+  vpc_id        = module.ops.opsvpc_id
+  owner_id      = data.aws_caller_identity.apps.account_id
+  peer_vpc_id   = module.ad.vpc_id
+  peer_owner_id = data.aws_caller_identity.apps.account_id
 }
 
 data "aws_vpc_peering_connection" "ad_peering" {
-  provider      = "aws.APPS"
-  vpc_id        = "${module.apps.appsvpc_id}"
-  owner_id      = "${data.aws_caller_identity.apps.account_id}"
-  peer_vpc_id   = "${module.ad.vpc_id}"
-  peer_owner_id = "${data.aws_caller_identity.apps.account_id}"
+  provider      = aws.APPS
+  vpc_id        = module.apps.appsvpc_id
+  owner_id      = data.aws_caller_identity.apps.account_id
+  peer_vpc_id   = module.ad.vpc_id
+  peer_owner_id = data.aws_caller_identity.apps.account_id
 }
 
 # data "aws_vpc_peering_connection" "ops_to_acpvpn" {
@@ -31,11 +31,11 @@ data "aws_vpc_peering_connection" "ad_peering" {
 # }
 
 data "aws_caller_identity" "apps" {
-  provider = "aws.APPS"
+  provider = aws.APPS
 }
 
 data "aws_caller_identity" "ci" {
-  provider = "aws.CI"
+  provider = aws.CI
 }
 
 data "aws_kms_secrets" "ad_joiner_password" {
@@ -43,7 +43,7 @@ data "aws_kms_secrets" "ad_joiner_password" {
     name    = "ad_joiner_password"
     payload = "AQICAHjC4CKFKTYsLki4xGd1rSMF7wMNMTmrxkezpb60vXSGtwGe2oPXUcDg/4HPEaxPaLuuAAAAcDBuBgkqhkiG9w0BBwagYTBfAgEAMFoGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMeCVXGJ7AUltYlqFRAgEQgC3NtnY3hGkL4qbo4aGyjYLrHODz+BCUUubQ5oC3PyiyJcL0z+7auFvAFAnMJzc="
 
-    context {
+    context = {
       terraform = "active_directory"
     }
   }
@@ -54,8 +54,9 @@ data "aws_kms_secrets" "ad_admin_password" {
     name    = "ad_admin_password"
     payload = "AQICAHjC4CKFKTYsLki4xGd1rSMF7wMNMTmrxkezpb60vXSGtwGe2oPXUcDg/4HPEaxPaLuuAAAAcDBuBgkqhkiG9w0BBwagYTBfAgEAMFoGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMeCVXGJ7AUltYlqFRAgEQgC3NtnY3hGkL4qbo4aGyjYLrHODz+BCUUubQ5oC3PyiyJcL0z+7auFvAFAnMJzc="
 
-    context {
+    context = {
       terraform = "active_directory"
     }
   }
 }
+
