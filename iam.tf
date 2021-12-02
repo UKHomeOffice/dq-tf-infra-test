bd8b3c5cb8d3a25e8c7ca8b3c9415b2e17b24295
JWanderema <wanderema@hotmail.co.uk>
2021-12-02 19:03:31 +0000

    add name

--- iam.tf
+++ iam.tf
@@ -121,7 +121,7 @@ EOF
 }
 
 resource "aws_iam_role" "test_role" {
-  name               = "test_role!"
+  name               = "test_role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
